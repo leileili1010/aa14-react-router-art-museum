808762f9c24fdc037f333196f7149ebd1837f2c4
Lei Li <lei.xw.li@hotmail.com>
2024-01-09 16:00:37 -0800

    phase 1 done

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -3,10 +3,20 @@ import harvardArt from './data/harvardArt'
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element:
+    <>
+      <h2>Harvard Art Museum</h2>
+      <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+    </> 
+  },
+  {
     path: "*",
     element: <h2>Page Not Found</h2>
   }
 ])
+
+
 
 function App() {
   return <RouterProvider router={router}/>;
